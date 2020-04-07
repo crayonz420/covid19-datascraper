9541231e0f732595207211cdf00e9d01815382c5
unknown <deadlyhunter295@gmail.com>
2020-04-07 15:26:21 -0700

    App port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const scraper = require('./scraper');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
