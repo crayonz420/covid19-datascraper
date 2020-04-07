3ef2df8a05506594d68727b7ac4484744af4f2a9
crayonz420 <38821021+crayonz420@users.noreply.github.com>
2020-04-07 14:55:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const scraper = require('./scraper');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
