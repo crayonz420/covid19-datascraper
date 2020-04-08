97cc4d925ef8abfac608d44f9e63f19a7a435161
unknown <deadlyhunter295@gmail.com>
2020-04-08 11:23:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,6 @@ const scraper = require('./scraper');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 app.use(express.static("public"));
 
 app.set("views", "./views");
