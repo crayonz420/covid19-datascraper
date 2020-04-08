aa9f48891c940586815c3e390ed25de931a9322d
unknown <deadlyhunter295@gmail.com>
2020-04-07 21:27:06 -0700

    Heroku Postgres cfg

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const scraper = require('./scraper');
-const client = require('redis').createClient(process.env.REDIS_URL);
 const app = express();
 const port = process.env.PORT || 8080;
+
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.use(express.static("public"));
 
